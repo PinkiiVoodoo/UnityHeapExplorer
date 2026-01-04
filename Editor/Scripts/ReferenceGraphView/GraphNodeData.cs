e2679555cb2d4d99bc112600b9e031ba2fc87dca
Gonzalo Diez Garrido <gonzalo@voodoo.io>
2026-01-04 18:31:42 +0700

    Draw a golden rectangle on root objects + add the reason to the subtitle

--- Editor/Scripts/ReferenceGraphView/GraphNodeData.cs
+++ Editor/Scripts/ReferenceGraphView/GraphNodeData.cs
@@ -13,6 +13,7 @@ namespace HeapExplorer
         public readonly RootPathUtility paths = new();
 
         public readonly RootPathReason rootReason;
+        public readonly bool isRoot;
 
         public HashSet<int> childNodes = new HashSet<int>();
 
@@ -25,13 +26,13 @@ namespace HeapExplorer
             this.position = position;
             this.objectProxy = objectProxy;
 
-            var isRoot = RootPathUtility.IsRoot(objectProxy, out rootReason);
+            isRoot = RootPathUtility.IsRoot(objectProxy, out rootReason);
             
             rect = new Rect(position, new Vector2(200, 80));
             isExpanded = false;
 
             title = CreateTitle(objectProxy);
-            subtitle = CreateSubtitle(objectProxy);
+            subtitle = CreateSubtitle(objectProxy) + (isRoot ? $"\n({rootReason})" : "");
             color = CreateColor(objectProxy);
         }
 
